f227324e685c0bb8af6930711d1ead8bb4faf716
Worick046 <pdwood02@gmail.com>
2024-12-10 14:57:15 -0800

    Added data loading capabilities.
    Added data loading that filters out certain files based on length. Also skips any files that have problems opening.

--- code/Course Project/AImusicAnalysis.py
+++ code/Course Project/AImusicAnalysis.py
@@ -3,7 +3,6 @@ import sounddevice as sd
 import numpy as np
 import torch
 from pandas import read_csv
-import mirdata
 
 run_mode = "IDE"
 if run_mode == "IDE":
@@ -15,9 +14,38 @@ else:
 def getWavfile(dataframe, index):
     return wavfile.read("./code/Course Project/TinySOL/" + dataframe['Path'].loc[dataframe.index[index]])
 
+def getWavfileSample(dataframe, index):
+    wavedata = getWavfile(dataframe, index)
+    return [wavedata[0], wavedata[1][wavedata[0]:wavedata[0] * 2]]
+
+def loadSamples(dataLabels):
+    data = []
+    instrumentTypes = dataLabels['Instrument (abbr.)'].unique()
+    labels = []
+    increment = 0
+    for i in range(len(dataLabels)):
+        try:
+            temp = getWavfileSample(dataLabels, i)
+        except:
+            continue
+        if len(temp[1]) == 44100:
+            print(increment, dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]], np.where(instrumentTypes == dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]]))
+            labels.append(np.where(instrumentTypes == dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]]))
+            data.append(temp)
+            increment += 1
+
+    return [data, labels]
+
+
 if __name__ == "__main__":
     dataLabels = read_csv(metadataPathName)
-    wavedata = getWavfile(dataLabels, 400)
-    print(wavedata)
-    sd.play(wavedata[1], wavedata[0])
+    print("Retrieving Sound Data")
+    data, labels = loadSamples(dataLabels)
+    print("Sound Data Retrived")
+    print(dataLabels['Instrument (abbr.)'].unique())
+    for i in range(len(labels)):
+        labels[i] = labels[i][0][0]
+
+    print(labels.index(1))
+    sd.play(data[labels.index(1) - 1][1], data[labels.index(1) - 1][0])
     sd.wait()
