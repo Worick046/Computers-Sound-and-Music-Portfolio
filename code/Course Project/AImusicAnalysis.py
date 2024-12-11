75f278a908fa12531ea2b3dbc5243572259186d9
Worick046 <pdwood02@gmail.com>
2024-12-10 17:02:57 -0800

    Built Convolutional Neural Network class
    Created a convolutional neural network class to classify 1 dimensional images. Also created a Fourier transform function and a normalization function.

--- code/Course Project/AImusicAnalysis.py
+++ code/Course Project/AImusicAnalysis.py
@@ -3,6 +3,8 @@ import sounddevice as sd
 import numpy as np
 import torch
 from pandas import read_csv
+import torch.nn as nn
+import torch.nn.functional as F
 
 run_mode = "IDE"
 if run_mode == "IDE":
@@ -16,7 +18,7 @@ def getWavfile(dataframe, index):
 
 def getWavfileSample(dataframe, index):
     wavedata = getWavfile(dataframe, index)
-    return [wavedata[0], wavedata[1][wavedata[0]:wavedata[0] * 2]]
+    return wavedata[1][wavedata[0]:wavedata[0] * 2]
 
 def loadSamples(dataLabels):
     data = []
@@ -28,13 +30,44 @@ def loadSamples(dataLabels):
             temp = getWavfileSample(dataLabels, i)
         except:
             continue
-        if len(temp[1]) == 44100:
-            print(increment, dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]], np.where(instrumentTypes == dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]]))
+        if len(temp) == 44100:
+            #print(increment, dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]], np.where(instrumentTypes == dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]]))
             labels.append(np.where(instrumentTypes == dataLabels['Instrument (abbr.)'].loc[dataLabels.index[i]]))
             data.append(temp)
             increment += 1
 
     return [data, labels]
+
+
+def performFourierTransform(waveform):
+    fft = np.fft.rfft(waveform)
+    amplitudes = abs(fft)
+    frequencies = np.fft.rfftfreq(len(waveform), d=1./44100)
+    return [frequencies, amplitudes]
+
+def NormalizeNdArray(array):
+    normalizedArray = np.array(array, dtype=np.float32)
+    normalizedArray = np.divide(normalizedArray, 32767)
+    return normalizedArray
+
+class NeuralNet1d(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv1d(1, 6, 6)
+        self.pool = nn.MaxPool1d(2, 2)
+        self.conv2 = nn.Conv1d(6, 16, 6)
+        self.fc1 = nn.Linear(11021, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 14)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 if __name__ == "__main__":
@@ -46,6 +79,11 @@ if __name__ == "__main__":
     for i in range(len(labels)):
         labels[i] = labels[i][0][0]
 
-    print(labels.index(1))
-    sd.play(data[labels.index(1) - 1][1], data[labels.index(1) - 1][0])
-    sd.wait()
+    for i in range(len(data)):
+        data[i] = NormalizeNdArray(data[i])
+
+    torchData = torch.from_numpy(np.array([data[0]]))
+    print(torchData.dtype)
+    print(torchData.shape)
+    Identifier = NeuralNet1d()
+    Identifier(torchData)
